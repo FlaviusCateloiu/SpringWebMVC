c73662fc7c84bf9e336fd8e9aaabb34e504b06a1
flaars <fcateloiu08@gmail.com>
2023-02-09 16:07:33 +0100

    Creacion del Proyecto de Spring

--- src/test/java/com/example/springwebmvc/SpringWebMvcApplicationTests.java
+++ src/test/java/com/example/springwebmvc/SpringWebMvcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringWebMvcApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
